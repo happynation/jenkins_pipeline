5d1638a38baa4e80ce806db4d7d9755a439eea65
vovatran1993 <vovatran1993@gmail.com>
2019-03-16 11:03:09 -0500

    more instruction added

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,20 @@
 node {
-    stage ("Pull git")
+    stage ("Pull git"){
     git "git@github.com:vovatran1993/website.git"
+    }
+    stage("Install apache"){
+    sh "ssh ec2-user@${DEVIP} sudo yum install httpd -y"
+    }
+
+    stage("Start Apache"){
+    sh "ssh ec2-user@${DEVIP}   sudo systemctl start httpd"
+    }
+
+    stage("Copy files"){
+    sh "rsync -aP --delete index.html  ec2-user@${DEVIP}:/tmp"
+    }
+
+    stage("Move files"){
+    sh "ssh ec2-user@${DEVIP}  sudo cp -f /tmp/index.html /var/www/html/index.html"
+    }
 }
